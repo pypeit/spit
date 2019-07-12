f6763f52fa0999c7ac6bed9e5442fd1783733f0f
Linyin Yang <lyang34@ucsc.edu>
2019-07-12 10:36:12 -0700

    Fixing learning rate
    when setting lr with adam = keras.optimizers.Adam(...), be sure to pass in parameter adam when compile, not 'adam'

--- spit/classifier.py
+++ spit/classifier.py
@@ -54,9 +54,9 @@ class Classifier(object):
     self.y_train = keras.utils.to_categorical(list(self.label_dict.values()), 
                                               num_classes=len(self.label_dict))
     # add optimizer, learning rate, and loss function
-    #adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 
-    self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])#, rate=1e-4)
+    self.model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])#, rate=1e-4)
     return
   
 
